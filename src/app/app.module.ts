8f7740f9d7bd2d39de71e9b6a8abccc673c2cb7f
Brett <brett7davis@gmail.com>
2017-10-22 11:04:43 +0200

    Add backend server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { Component, OnInit, OnDestroy, ElementRef, ViewChild } from '@angular/core/';
 import { NgModule } from '@angular/core';
-
+import {environment} from '../environments/environment'
+import {Ovservable} from 'rxjs/Observable';
+import {Subject} from 'rxjs/Subject';
+import 'rxjs/add/operator/takeUntil';
+import 'rxjs/add/operator/take';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/retryWhen';
+import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/delayWhen';
+import 'rxjs/add/observable/zip';
+import 'rxjs/add/observable/range';
+import 'rxjs/add/observable/timer';
+import 'rxjs/add/observable/interval';
+import 'rxjs/add/observable/dom/webSocket';
+import * as moment from 'moment';
 import { AppComponent } from './app.component';
 
 @NgModule({
